e39c7ece2ab02ea0ee786367168ebdac2aa77383
Ehorga <yehornahiba1207@gmail.com>
2025-04-29 00:57:02 +0300

    changes

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("hi");
+
+let firstName = "Olena";
+firstName = "Yehor";
+console.log(firstName);
+
+let favoriteColor = "Green";
+favoriteColor = "blue";
+console.log(favoriteColor);
+
+let isAdult = true;
+let isweekend = false;
+let isStudent = false;
+
+let pet = "null";
+console.log(pet);
+const timewakeup = "7.30"
+console.log(timewakeup)
