2b713daeeda94f60b502b410f9f783c2a655d623
Ehorga <yehornahiba1207@gmail.com>
2025-04-29 16:27:47 +0300

    practic-29-04-25

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const userName = prompt('Ведить ваше ім\'я');
-console.log(userName)
-alert('привіт,' + userName + '!'  )
+const author = "King";
+const bookTitle = "It";
+const pageCount = 1246;
+// alert(
+//   'My favorite book :\n "' +
+//     bookTitle +
+//     '" by  ' +
+//     author +
+//     " (" +
+//     pageCount +
+//     " pages )"
+// );
+const product = "Laptop";
+const price = 57000;
+const amount = 3;
+const cost = price * amount;
+// alert("The cost of " + amount + ' items "' + product + '" is ' + cost + " UAH");
+// const userName = prompt("enter your name");
+// const city = prompt("enter city");
+// alert("hi, " + userName + "! Nice day :) Welcome in " + city +"!");
+// const task1 = prompt("enter first task");
+// const task2 = prompt("enter second task");
+// alert("My plans for day:\n1) " + task1 + " \n2) " + task2)
+const isWinOscar = false;
+const isRainy = true;
+const isPasportReady = true;
