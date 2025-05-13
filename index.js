7021910a6df5cbc1d4c69ac5bce621fbc702f13c
Ehorga <yehornahiba1207@gmail.com>
2025-05-13 16:30:00 +0300

    practic13-05-25

--- index.js
+++ index.js
@@ -1,22 +1,91 @@
-const num1 = 5;
-const num2 = 8;
-const num3 = 8;
-
-const str1 = '5';
-const str2 = 'cat';
-const str3 = 'dog';
-
-console.log(num1 > num2);  
-console.log(num1 < num2);   
-
-console.log(num1 >= num2);  
-console.log(num1 <= num2); 
-
-console.log(num3 >= num2);  
-console.log(num3 <= num2);  
-
-console.log(num1 == num2); 
-console.log(num3 == num2); 
-
-console.log(num1 != num2);  
-console.log(num3 != num2);  
+// const ADULT = 18;
+// const userAge = prompt("How many years old are you?");
+// if (userAge >= ADULT) {
+//   console.log("you adult")
+// } else {
+//   console.log("you not adult")
+// }
+// const number = prompt("enter number");
+// if (number >= 0) {
+//   console.log("positive")
+// } else {
+//   console.log("negative")
+// }
+// if (number < 0) {
+//   console.log("negative")
+// } else {
+//   console.log("positive")
+// }
+const num1 = prompt("enter first number");
+// const num2 = prompt("enter second number");
+// if (num1 > num2) {
+//   console.log(num1)
+// } else if(num2 > num1) {
+//   console.log(num2)
+// } else {
+//   console.log("simmilar")
+// }
+// const sign = prompt("enter + or - or * or /")
+// if (sign === "+") {
+//   console.log(Number(num1) + Number(num2))
+// } else if (sign === "-"){
+//   console.log(num1 - num2)
+// } else if (sign === "*"){
+//   console.log(num1 * num2)
+// }else if (sign === "/"){
+//   console.log(num1 / num2)
+// }else {
+//   console.log("(")
+// }
+// if (num1 === num2) {
+//   console.log("simmilar")
+// } else {
+//   console.log("asimmilar")
+// }
+// const sum = Number(num1) + Number(num2);
+// const mult = num1 * num2;
+// if (sum >= mult) {
+//   console.log("win")
+// } else {
+//   console.log("lose")
+// }
+// const userN = prompt("enter something");
+// if (userN === "") {
+//   console.log("lose");
+// } else if (userN === null) {
+//   console.log("lose");
+// } else {
+//   console.log("win");
+// }
+// console.log(userN);
+const PASSWORD = "1234";
+const userPassword = prompt("enter password");
+if (PASSWORD !== userPassword) {
+  console.log("lose");
+  
+} else {
+  console.log("win");
+  
+}
+// if (PASSWORD === userPassword) {
+//   console.log("win");
+// } else {
+//   console.log("lose");
+// }
+// if (num1 === num2) {
+//   if (num1 > 0) {
+//       console.log(" thisis square");
+//   }
+//   else{
+//     console.log("not square");
+//   }
+// } else {
+//   console.log("not square");
+// }
+// if (num1 <= 3 ) {
+//   console.log("win");
+  
+// } else {
+//   console.log("lose");
+  
+// }
