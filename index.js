255d6b9a14594ea832adffff5bf58c463b5048fe
Ehorga <yehornahiba1207@gmail.com>
2025-05-20 01:51:56 +0300

    25-05-20

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
- const userAgeInput = prompt('Enter your age'); 
-const startSchoolAge = 6;
-const endSchoolAge = 17;
 
-const userAge = Number(userAgeInput);
 
-if (userAge >= startSchoolAge && userAge <= endSchoolAge) {
-    alert('school'); 
+const userInput1 = prompt('Enter number 1');
+const userInput2 = prompt('Enter number 2');
+
+const number1 = Number(userInput1);
+const number2 = Number(userInput2);
+
+if (number1 > 0 && number2 > 0) {
+    alert('Обидва числа позитивні');
+} else if (number1 < 0 && number2 < 0) {
+    alert('Обидва числа негативні');
+} else if ((number1 > 0 && number2 < 0) , (number1 < 0 && number2 > 0)) {
+    alert('Числа різні за знаком');
 } else {
-    alert('not school'); 
+    alert('Принаймні одне з чисел — нуль');
 }
