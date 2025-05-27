8cd3e9a2725fb4b1662d9f469677d992a3a9949d
Ehorga <yehornahiba1207@gmail.com>
2025-05-27 15:22:45 +0300

    switch

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-// 1)// const age = prompt("Введите ваш возраст:");
-// // const numericAge = Number(age);
-// // const message = numericAge >= 18 ? "Вы совершеннолетний." : "Вы несовершеннолетний.";
-// // alert(message);
-// 2)const userAgeInput = prompt('Введіть ваш вік:');
-// const userAge = Number(userAgeInput);
+const message = `Введи мову, якою хочеш спілкуватися:
+українська - ua
+англійська - en
+польська - pl
+іспанська - es
+французька - fr
+`;
 
-// if (userAge < 0) {
-//     alert('Некоректний вік');
-// } else if (userAge < 12) {
-//     alert('Дитина');
-// } else if (userAge < 18) {
-//     alert('Підліток');
-// } else if (userAge < 65) {
-//     alert('Дорослий');
-// } else {
-//     alert('Старший');
-// }
-// 3)const userHourInput = prompt('Котра година?');
-// const userHour = Number(userHourInput);
+const lang = prompt(message);
 
-// if (userHour < 0 || userHour >= 24) {
-//     alert('Не буває такої години');
-// } else if (userHour < 5) {
-//     alert('Доброї ночі');
-// } else if (userHour < 12) {
-//     alert('Доброго ранку');
-// } else if (userHour < 18) {
-//     alert('Доброго дня');
-// } else if (userHour < 22) {
-//     alert('Доброго вечора');
-// } else {
-//     alert('Доброї ночі');
-// }
+if (lang === 'ua') {
+    alert('привіт');
+} else if (lang === 'en') {
+    alert('hi');
+} else if (lang === 'pl') {
+    alert('cześć');
+} else if (lang === 'es') {
+    alert('hola');
+} else if (lang === 'fr') {
+    alert('bonjour');
+} else {
+    alert('мова не підтримується');
+}
+switch (lang) {
+  case 'ua':
+    alert('привіт');
+    break;
+  case 'en':
+    alert('hi');
+    break;
+  case 'pl':
+    alert('cześć');
+    break;
+  case 'es':
+    alert('hola');
+    break;
+  case 'fr':
+    alert('bonjour');
+    break;
+  default:
+    alert('мова не підтримується');
+    break;
+}
