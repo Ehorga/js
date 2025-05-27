8e300b86634a61977026729b54da131853b438ac
Ehorga <yehornahiba1207@gmail.com>
2025-05-27 16:30:19 +0300

    practic25-05-27

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-const message = `Введи мову, якою хочеш спілкуватися:
-українська - ua
-англійська - en
-польська - pl
-іспанська - es
-французька - fr
-`;
+// const dayNumber = 9;
+// if (dayNumber === 6 || dayNumber === 7) {
+//   console.log("weekend");
+// } else if (dayNumber >= 1 && dayNumber <= 5) {
+//   console.log("work day");
+// } else {
+//   console.log("wrong data");
+// }
+// switch (dayNumber) {
+//   case 6:
+//   case 7:
+//     console.log("weekend");
+//     break;
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("work day");
+//     break;
+//   default:
+//     console.log("wrong data");
+//     break;
+// }
+// const time = 1.19;
+// if (time >= 12 && time <= 13.5) {
+//   console.log("lunch");
+// } else {
+//   console.log("not lunch");
+// }
+// console.log(time >= 12 && time <= 13.5 ? "lunch" : "not lunch");
+// const user1 = "rck";
+// const user2 = "rock";
+// console.log("first " + user1);
+// console.log("second " + user2);
+// if (user1 === user2) {
+//   console.log("draw");
+// } else if (
+//   (user1 === "paper" && user2 === "rock") ||
+//   (user1 === "scissors" && user2 === "paper") ||
+//   (user1 === "rock" && user2 === "scissors")
+// ) {
+//   console.log("first win");
+// } else {
+//   console.log("second win");
+// }
+// const cycleAmount = 6;
+// switch (cycleAmount) {
+//   case 1:
+//     console.log("monocycle");
 
-const lang = prompt(message);
+//     break;
+//   case 2:
+//     console.log("bicycle");
+//     break;
+//   case 4:
+//     console.log("auto");
+//     break;
+//   case 6:
+//     console.log("bus");
+//     break;
+//   case 18:
+//     console.log("fura");
+//     break;
 
-if (lang === 'ua') {
-    alert('привіт');
-} else if (lang === 'en') {
-    alert('hi');
-} else if (lang === 'pl') {
-    alert('cześć');
-} else if (lang === 'es') {
-    alert('hola');
-} else if (lang === 'fr') {
-    alert('bonjour');
-} else {
-    alert('мова не підтримується');
-}
-switch (lang) {
-  case 'ua':
-    alert('привіт');
-    break;
-  case 'en':
-    alert('hi');
-    break;
-  case 'pl':
-    alert('cześć');
-    break;
-  case 'es':
-    alert('hola');
-    break;
-  case 'fr':
-    alert('bonjour');
-    break;
-  default:
-    alert('мова не підтримується');
-    break;
-}
+//   default:
+//     console.log("idk");
+
+//     break;
+// }
