385fbf23749c213fe41cfd78fe8fc410b525eeba
Ehorga <yehornahiba1207@gmail.com>
2025-06-03 14:36:40 +0300

    for+

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// const password = '123qwe';
+// const maxTry = 3;
+
+// for (let i = 0; i < maxTry; i++) {
+//     const userInputPass = prompt('Enter password');
+
+//     if (userInputPass === password) {
+//         alert('Welcome!');
+//         break; // выход из цикла при правильном пароле
+//     }
+
+//     alert('У вас лишилося ще ' + (maxTry - 1 - i) + ' спроби');
+// }
+
