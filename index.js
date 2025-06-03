9a2a04225313232a2a7a0c80dc6e23a19ef4c8b1
Ehorga <yehornahiba1207@gmail.com>
2025-06-03 16:31:26 +0300

    practic25-06-03

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-const start = 15;
-const end = 20;
-const secretNumber = 17;
+// const userNumber = prompt("enter your number");
+// let number = Number(userNumber);
+// let sign = prompt("enter + or -");
+// while (sign !== null) {
+//   switch (sign) {
+//     case "+":
+//       number++;
+//       break;
+//     case "-":
+//       number--;
+//       break;
+//     default:
+//       console.log("atention");
 
-for (let i = start; i <= end; i++) {
-    if (i === secretNumber) {
-        continue; 
-    }
+//       break;
+//   }
+//   sign = prompt("enter + or -");
+// }
+// console.log(number);
+//  let score = 0
+// while (score < 3) {
+//     const userNumber = prompt("enter your number");
+//     let number = Number(userNumber);
+//     if (number%7 === 0) {
+//         score++
+//     }
+
+// }
+// console.log(score);
+// for (let i = 3; i <= 9; i++) {
+//     console.log(i, "->" , i*i);
+
+// }
+// for (let i = 30; i > 10; i--) {
+//   if (i % 6 === 0) {
+//     console.log(i);
+//   }
+// }
+//  for (let i = 30; i > 10; i--) {
+//   if (i % 6 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+for (let i = 1; i <= 20; i++) {
+  if (i % 4 === 0) {
+    console.log("***");
+  } else if (i % 6 === 0) {
+    console.log("$$$");
+  } else {
     console.log(i);
-}
-while (true) {
-    const userInput = prompt('Enter number');
-
-    if (userInput === null) {
-        alert('Будь уважний!');
-        continue; 
-    }
-
-    if (Number(userInput) === secretNumber) {
-        alert('Win!');
-        break; 
-    }
+  }
 }
