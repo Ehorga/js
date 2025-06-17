a0cc112591e6cc877c20a654ee471e56896b2e34
Ehorga <yehornahiba1207@gmail.com>
2025-06-17 13:12:28 +0300

    function

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-// const userNumber = prompt("enter your number");
-// let number = Number(userNumber);
-// let sign = prompt("enter + or -");
-// while (sign !== null) {
-//   switch (sign) {
-//     case "+":
-//       number++;
-//       break;
-//     case "-":
-//       number--;
-//       break;
-//     default:
-//       console.log("atention");
 
-//       break;
-//   }
-//   sign = prompt("enter + or -");
-// }
-// console.log(number);
-//  let score = 0
-// while (score < 3) {
-//     const userNumber = prompt("enter your number");
-//     let number = Number(userNumber);
-//     if (number%7 === 0) {
-//         score++
-//     }
-
-// }
-// console.log(score);
-// for (let i = 3; i <= 9; i++) {
-//     console.log(i, "->" , i*i);
-
-// }
-// for (let i = 30; i > 10; i--) {
-//   if (i % 6 === 0) {
-//     console.log(i);
-//   }
-// }
-//  for (let i = 30; i > 10; i--) {
-//   if (i % 6 !== 0) {
-//     continue;
-//   }
-//   console.log(i);
-// }
-for (let i = 1; i <= 20; i++) {
-  if (i % 4 === 0) {
-    console.log("***");
-  } else if (i % 6 === 0) {
-    console.log("$$$");
-  } else {
-    console.log(i);
-  }
+function logHi() {
+    console.log('Hi!');
 }
+logHi();
+
+if (5 === 2) {
+    logHi();
+}
+
