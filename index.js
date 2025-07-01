7d2c108c1288424fe5e1423c69590924d03a98ef
Ehorga <yehornahiba1207@gmail.com>
2025-07-01 15:25:14 +0300

    array

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-// Порождаем пустой объект
-const obj = {};
-console.log(obj);
+
+const colors = ['red', 'orange', 'yellow', 'green'];
+console.log(colors); 
 
 
-function User(name, age, isMale) {
-  this.name = name;
-  this.age = age;
-  this.isMale = isMale;
-}
+const numbers = new Array(6, 25, 13, 41);
+console.log(numbers); 
 
-
-const UserPrototype = {
-  getInfo() {
-    return `${this.name} is ${this.age} years old`;
-  }
-};
-
-
-User.prototype = UserPrototype;
-
-
-const user1 = new User('Alex', 20, true);
-console.log(user1);
-console.log(user1.getInfo()); 
-
-const user2 = new User('Maria', 23, false);
-console.log(user2);
-console.log(user2.getInfo()); 
