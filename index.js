2ebfb63972342a16306282ff84e3d0e11e2f28ef
Ehorga <yehornahiba1207@gmail.com>
2025-07-08 16:31:58 +0300

    practic-08-07-25

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const site = {
-  title: 'my site',
-  headers: ['qwe1', 'qwe2', 'qwe3'],
+// const tasks =['clean room','water flowers','learn JS' ];
+// const userAnswer= prompt("what want to do");
+// if (tasks.includes(userAnswer)) {
+//   const index = tasks.indexOf(userAnswer)
+//   tasks.splice(index , 1)
+// } else {
+//   // tasks.unshift(userAnswer)
+//   tasks.splice(0, 0 ,userAnswer)
+// }
+// console.log(tasks);
+const numbers =[1,2,3,4,5,6,7,8];
+// const first = numbers.shift();
+// const last = numbers.pop();
+// numbers.unshift(last);
+// numbers.push(first)
+// console.log(numbers);
+// const third =numbers.splice(3,1).shift()
+// numbers.push(third)
+// console.log(numbers);
+// console.log(third);
+// const userNumber =prompt("enter number")
+// const userIndex = prompt("enter index")
+// numbers.splice(Number(userIndex)  , 0 , Number(userNumber))
 
-  showHeaders() {
-    this.headers.forEach(function(elem) {
-      console.log(elem);
-    });
-  },
+numbers.splice(numbers.length-2,2 )
 
-  showHeadersAndTitle() {
-    this.headers.forEach((elem) => {
-      console.log(elem, this.title);
-    });
-  },
-
-  showHeadersAndTitle2() {
-    this.headers.forEach(function(elem) {
-      console.log(elem, this.title);
-    });
-  }
-};
-
-site.showHeaders();
-site.showHeadersAndTitle();
-site.showHeadersAndTitle2();
+console.log(numbers);
