c381bc3e6d8fbbc9fa44100c07aa4468dfeef87e
Ehorga <yehornahiba1207@gmail.com>
2025-07-15 02:09:40 +0300

    arrow practic

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-// const tasks =['clean room','water flowers','learn JS' ];
-// const userAnswer= prompt("what want to do");
-// if (tasks.includes(userAnswer)) {
-//   const index = tasks.indexOf(userAnswer)
-//   tasks.splice(index , 1)
-// } else {
-//   // tasks.unshift(userAnswer)
-//   tasks.splice(0, 0 ,userAnswer)
-// }
-// console.log(tasks);
-const numbers =[1,2,3,4,5,6,7,8];
-// const first = numbers.shift();
-// const last = numbers.pop();
-// numbers.unshift(last);
-// numbers.push(first)
-// console.log(numbers);
-// const third =numbers.splice(3,1).shift()
-// numbers.push(third)
-// console.log(numbers);
-// console.log(third);
-// const userNumber =prompt("enter number")
-// const userIndex = prompt("enter index")
-// numbers.splice(Number(userIndex)  , 0 , Number(userNumber))
-
-numbers.splice(numbers.length-2,2 )
-
+const numbers = [5, 10, 15, 20, 25, 30, 35];
 console.log(numbers);
+
+// const lastNumber = numbers.pop();
+// numbers.unshift(lastNumber);
+// console.log(numbers);
+
+// const firstElement = numbers.shift();
+// numbers.push(firstElement);
+// console.log(numbers);
+
+numbers.forEach(function(elem) {
+  console.log(elem);
+});
