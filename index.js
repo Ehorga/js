3f149207174167ec56c50f19412e23ffa035e18b
Ehorga <yehornahiba1207@gmail.com>
2025-07-15 16:30:21 +0300

    practic-15-07-25

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const str1 = 'cat and dog';
-console.log(str1);
-console.log(str1.length);
+// const carMarks = ["Audi", "Bmw", "Reno"];
+// const newCar = prompt("Enter new mark auto");
+// if (carMarks.includes(newCar)) {
+//   console.log("Така марка вже є");
+// } else {
+//   carMarks.forEach((elem, index) => {
+//     console.log(index, "-", elem);
+//   });
+//   const pos = prompt("після якої позиції додати нову марку в масив?")
+//   carMarks.splice(Number(pos) + 1 , 0,newCar)
+// }
+// carMarks.forEach((elem, index) => {
+//     console.log(index, "-", elem);
+//   });
+const numbers = [1, 2, 3, 4, 5, 6];
+// const moved =numbers.splice(1,1).pop()
+// numbers.push(moved)
+// console.log(numbers);
+// let count = 0;
+// numbers.forEach((elem, index) => {
+//   if (elem % 3 === 0) {
+//     count++;
+//   }
+// });
+// console.log(count);
+// numbers.forEach((elem ,index )=> {
+//     if (elem % 2 !== 0) {
+//         console.log(elem);
 
-console.log(str1.includes('and'));
-
-const firstLetter = str1.charAt(0);
-console.log(firstLetter);
-
-const lastLetter = str1.at(-1);
-console.log(lastLetter);
-
-console.log(str1.indexOf('cat'));
-console.log(str1.lastIndexOf('cat'));
+//     }
+// })
+// const newNumbers = numbers.map((elem, index) => {
+//   return elem - 4;
+// });
+// console.log(newNumbers);
+// const newNumbers = numbers.map((elem) => elem - 4);
+// console.log(newNumbers);
+const newNumbers = numbers.map((elem, index) =>
+  elem % 2 === 0 ? elem + 7 : elem
+);
+console.log(newNumbers);
