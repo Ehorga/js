a31a4a2b692cd4beb984ee84af5d805d852229b4
Ehorga <yehornahiba1207@gmail.com>
2025-07-22 01:43:37 +0300

    sort & reverse

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-// const carMarks = ["Audi", "Bmw", "Reno"];
-// const newCar = prompt("Enter new mark auto");
-// if (carMarks.includes(newCar)) {
-//   console.log("Така марка вже є");
-// } else {
-//   carMarks.forEach((elem, index) => {
-//     console.log(index, "-", elem);
-//   });
-//   const pos = prompt("після якої позиції додати нову марку в масив?")
-//   carMarks.splice(Number(pos) + 1 , 0,newCar)
-// }
-// carMarks.forEach((elem, index) => {
-//     console.log(index, "-", elem);
-//   });
-const numbers = [1, 2, 3, 4, 5, 6];
-// const moved =numbers.splice(1,1).pop()
-// numbers.push(moved)
-// console.log(numbers);
-// let count = 0;
-// numbers.forEach((elem, index) => {
-//   if (elem % 3 === 0) {
-//     count++;
-//   }
-// });
-// console.log(count);
-// numbers.forEach((elem ,index )=> {
-//     if (elem % 2 !== 0) {
-//         console.log(elem);
+const number = 11111;
+console.log(number, typeof number);
 
-//     }
-// })
-// const newNumbers = numbers.map((elem, index) => {
-//   return elem - 4;
-// });
-// console.log(newNumbers);
-// const newNumbers = numbers.map((elem) => elem - 4);
-// console.log(newNumbers);
-const newNumbers = numbers.map((elem, index) =>
-  elem % 2 === 0 ? elem + 7 : elem
-);
-console.log(newNumbers);
+const numToStr = number.toString();
+console.log(numToStr, typeof numToStr);
+
+const arrStr = numToStr.split('');
+console.log(arrStr, typeof arrStr);
+
+const arrNums = arrStr.map(elem => Number(elem));
+console.log(arrNums, typeof arrNums);
+
+const summa = arrNums.reduce((acc, elem) => acc + elem, 0);
+console.log(summa, typeof summa);
