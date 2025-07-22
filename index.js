aa36a46722c5c1a7edfafbff73612e8cc33df376
Ehorga <yehornahiba1207@gmail.com>
2025-07-22 16:34:16 +0300

    practice-22-07-25

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// rest
-function summa(...args) {
-  console.log(args); 
-  return args.reduce((accumulator, elem) => accumulator + elem, 0);
+const userInput = "enter something enter";
+console.log(userInput, userInput.length);
+const word1 =
+  userInput[0].toUpperCase() +
+  userInput.slice(1, -1).toLowerCase() +
+  userInput.at(-1).toUpperCase();
+console.log(word1);
+function checkSymbolInRow(str, symbol) {
+  return str.includes(symbol);
 }
 
-console.log(summa(1)); 
-
-const numbers = [1, 5, 9, 3, 1];
-
-// spread
-console.log(summa(...numbers)); 
+console.log(checkSymbolInRow(userInput, "o"));
+const countWord = userInput.split(" ").length;
+console.log(countWord);
+const arrayWord = userInput.split(" ");
+const object = {};
+for (let index = 0; index < arrayWord.length; index++) {
+  const element = arrayWord[index];
+  object[element] = element.length;
+}
+console.log(object);
