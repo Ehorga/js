9aabfd871434db451522305efeb7456aec64b392
Ehorga <yehornahiba1207@gmail.com>
2025-08-12 16:28:56 +0300

    practic-12-08-25

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
-class User {
-    #login
-    constructor(login) {
-        this.login = login;
+class Rect {
+    constructor(height , width){
+        this.height = height;
+        this.width = width;
     }
-    set login(value) {
-        if (typeof value !== 'string') throw new TypeError('type must be string');
-        if (value.trim().length < 3 || value.trim().length > 15) throw new RangeError('length 3..15');
-        this.#login = value;
-    }
-    get login() {
-        return this.#login;
+    getArea(){
+        return this.width * this.height
     }
 }
-
-try {
-    const user = new User('Anna');
-    console.log(user);
-} catch (error) {
-    console.error(error);
+const rect1 =new Rect ( 10 ,20)
+// console.log(rect1.getArea());
+// console.log(rect1);
+class Message {
+    constructor(text){
+        this.text =text;
+    }
+    uppercase(){
+        return this.text.toUpperCase();
+    }
+    getLength(){
+        return this.text.length;
+    }
 }
-
-console.log('code');
+const message1 = new Message ("hello world")
+// console.log(message1.uppercase());
+// console.log(message1.getLength());
+class Timer{
+    constructor(seconds= 0){
+  this.seconds =seconds;
+    }
+    tick(){
+        this.seconds++
+    }
+    showSeconds(){
+        return this.seconds;
+    }
+}
+const timer = new Timer()
+timer.tick()
+// console.log(timer.showSeconds());
+class Ticket {
+    #price;
+    constructor(price){
+        this.price=price;
+    }
+    set price(value){
+        if (typeof value !== "number"){
+            throw new TypeError('price must be number')
+        }
+        if (value < 0){
+            throw new RangeError( ' price mmust be positive')
+        }
+        this.#price=value
+    }
+    get price() {
+        return this.#price
+    }
+}
+// const ticket =new Ticket(-20)
+// // console.log(ticket.price);
+class Powerbank {
+    #charge
+    constructor(charge){
+        this.charge=charge 
+    }
+    set charge (value){
+        if (typeof value !== "number"){
+            throw new TypeError (' charge must be number')
+        }
+        if(value <0){
+            throw new RangeError ('value must be more than 0')
+        }
+        this.#charge = value
+    }
+    get charge(){
+        return this.#charge
+    }
+}
+const powerbank = new Powerbank(30)
+console.log(powerbank.charge);
