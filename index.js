3860951b6252f4ceebbb0271ca40e2482cf3ca15
Ehorga <yehornahiba1207@gmail.com>
2025-08-26 01:58:54 +0300

    HTMLCollection

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const h1 = document.getElementById('main-title');
-h1.className = 'heading';
-h1.textContent = 'hi from javascript!';
-h1.title = 'title for main title';
+const bigImg = document.getElementById('big');
+const smallImgs = document.getElementsByClassName('small');
 
-const img = document.getElementById('image');
-img.src = 'https://www.earth.com/_next/image/?url=https%3A%2F%2Fcfff2.earth.com%2Fuploads%2F2023%2F06%2F1608040%2Fsea-turtle3_medium-960x640.jpg&w=3840&q=75';
-img.alt = 'turtle';
-img.width = 400;
-img.height = 400;
-img.style.objectFit = 'cover';
+for (const smallImg of smallImgs) {
+  smallImg.addEventListener('click', () => {
+    bigImg.src = smallImg.src;
+  });
+}
+
+const lisFruites = document.getElementsByTagName('li');
+
+for (const li of lisFruites) {
+  li.addEventListener('click', () => {
+    li.style.backgroundColor =
+      li.style.backgroundColor === 'pink' ? '' : 'pink';
+  });
+}
