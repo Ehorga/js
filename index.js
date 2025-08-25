37f52346e6fe8bc245c3ab56773fa3138a25bd27
Ehorga <yehornahiba1207@gmail.com>
2025-08-26 01:58:25 +0300

    addEventListener

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const obj = {
-  prop: 12,
-  hi() {
-    return 'hi';
-  }
-};
-
-console.log(obj);
-console.dir(document);
-
 const h1 = document.getElementById('main-title');
+h1.className = 'heading';
+h1.textContent = 'hi from javascript!';
+h1.title = 'title for main title';
 
-console.log(h1);
-console.dir(h1);
+const img = document.getElementById('image');
+img.src = 'https://www.earth.com/_next/image/?url=https%3A%2F%2Fcfff2.earth.com%2Fuploads%2F2023%2F06%2F1608040%2Fsea-turtle3_medium-960x640.jpg&w=3840&q=75';
+img.alt = 'turtle';
+img.width = 400;
+img.height = 400;
+img.style.objectFit = 'cover';
