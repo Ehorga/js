8405303cb3e6713ea7136853df10e30334983aca
Ehorga <yehornahiba1207@gmail.com>
2025-09-29 23:13:32 +0300

    setInterval, setTimeout

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const root = document.getElementById('root');
-const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"];
 
-const btns = colors.map(color => {
-  const btn = document.createElement('button');
-  btn.textContent = color;
-  btn.style.backgroundColor = color;
-  return btn;
-});
-
-root.append(...btns);
+setTimeout(() => {
+  const subscribe = document.createElement('section');
+  const close = document.createElement('span');
+  close.textContent = 'X';
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Subscribe to our email newsletter';
+  const form = document.createElement('form');
+  const input = document.createElement('input');
+  const button = document.createElement('button');
+  button.textContent = 'Subscribe';
+  form.append(input, button);
+  subscribe.append(close, h2, form);
+  root.append(subscribe);
+}, 5000);
