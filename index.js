478f3131b8b4ded651de2478929532db345afff8
Ehorga <yehornahiba1207@gmail.com>
2025-10-14 00:05:56 +0300

    JS: fetch practice

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const myPromise = new Promise((resolve, reject) => {
-  const random = Math.random();
-  if (random > 0.5) {
-    resolve('All good! random = ' + random);
-  } else {
-    reject('Error! random = ' + random);
-  }
-});
+const root = document.getElementById('root');
+const limit = 10;
 
-myPromise
+const container = document.createElement('div');
+container.classList.add('container');
+root.append(container);
+
+fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}`)
   .then((response) => {
-    console.log(response);
+    // JSON — возвращает Promise с массивом данных
+    return response.json();
   })
-  .catch((err) => {
-    console.log(err);
+  .then((dataArray) => {
+    // Создание массива элементов img
+    const imgArray = dataArray.map((element) => {
+      const img = document.createElement('img');
+      img.src = element.url;
+      img.classList.add('img');
+
+      // Обработчик клика — открыть большое изображение
+      img.addEventListener('click', () => {
+        const bigImgWrapper = document.createElement('div');
+        bigImgWrapper.classList.add('bigImgWrapper');
+
+        const bigImg = document.createElement('img');
+        bigImg.src = element.url;
+
+        const close = document.createElement('span');
+        close.textContent = 'X';
+        close.classList.add('close');
+
+        close.addEventListener('click', () => {
+          bigImgWrapper.remove();
+        });
+
+        bigImgWrapper.append(close, bigImg);
+        root.append(bigImgWrapper);
+      });
+
+      return img;
+    });
+
+    container.append(...imgArray);
   })
-  .finally(() => {
-    console.log('finally');
+  .catch((error) => {
+    console.log('Ошибка при загрузке изображений:', error);
   });
